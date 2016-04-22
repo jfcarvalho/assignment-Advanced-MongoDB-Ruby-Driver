3cf0fe0d17b4de362d8633a76bf866a226cb96d6
jfcarvalho <jfcarvalho@dcc.ufba.br>
2016-04-22 09:49:40 -0300

    Requisito 02 Collection funcionando

--- places/app/models/place.rb
+++ places/app/models/place.rb
@@ -1,3 +1,28 @@
 class Place
   include Mongoid::Document
+  include ActiveModel::Model
+ 
+  require 'mongo'
+  require 'pp'
+  require 'byebug'
+  require 'uri'
+  require 'json'
+
+ @@db = nil
+ 
+ def self.mongo_client
+    @@db = Mongo::Client.new('mongodb://localhost:27017/places_development')
+ end
+
+def self.collection
+    self.mongo_client if not @@db
+    @@db[:places]
+end
+
+def self.load_all io
+	file_json = JSON.parse(io.read)
+	self.collection.insert_many(file_json)
+end	
+
+
 end
